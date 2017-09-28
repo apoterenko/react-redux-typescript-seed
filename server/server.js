40eb53be697dab197e2004557c34907b2bef3c61
apoterenko <a.poterenko@gmail.com>
2017-09-28 18:15:48 +0300

    *custom server.js

---
+++ server/server.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var jsonServer = require('json-server');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var router = jsonServer.router('server/data.json');
+var jsonConfig = JSON.parse(fs.readFileSync('server/data.json', 'utf8'));
+var middlewares = jsonServer.defaults();
+var app = express();
+
+app.use(middlewares);
+app.use(bodyParser.json());
+app.use(function (req, res) {
+	setTimeout(function () {
+		var params = req.body;
+		var data = jsonConfig[params.name];
+		res.send(JSON.stringify({result: data === undefined ? true : data}));
+	}, 1000);
+});
+
+app.use('/api', router);
+
+https.createServer({}, app).listen(8443, function () {
+	console.log('|-----------------------------------------------------------------------------------|');
+	console.log('| The server is listening on a 8443 port...                                         |');
+	console.log('| An application is available on the link "https://localhost:8443/api/"             |');
+	console.log('|-----------------------------------------------------------------------------------|');
+	console.log();
+});
