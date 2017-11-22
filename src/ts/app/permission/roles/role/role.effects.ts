e82cdc4f385b9e22ed761e120e42c50ac5388d33
aleksandr.poterenko <aleksandr.poterenko@walli.com>
2017-11-22 19:23:12 +0300

    *optimization

--- src/ts/app/permission/roles/role/role.effects.ts
+++ src/ts/app/permission/roles/role/role.effects.ts
@@ -5,29 +5,28 @@ import {
   FormActionBuilder,
   IApiEntity,
   BaseEffects,
+  makeSucceedFormEffectsProxy,
+  makeFailedFormEffectsProxy,
+  effectsBy,
 } from 'react-application-core';
 
-import { ROUTER_PATHS } from '../../../app.routers';
 import { ROLES_SECTION } from '../roles.interface';
 import { ROLE_SECTION } from './role.interface';
 import { IApi } from '../../../api/api.interface';
 import { IRoleEntity } from '../../permission.interface';
 
 @provideInSingleton(RoleEffects)
+@effectsBy(
+    makeFailedFormEffectsProxy(ROLE_SECTION),
+    makeSucceedFormEffectsProxy({
+      listSection: ROLES_SECTION,
+      formSection: ROLE_SECTION,
+    })
+)
 export class RoleEffects extends BaseEffects<IApi> {
 
   @EffectsService.effects(FormActionBuilder.buildSubmitActionType(ROLE_SECTION))
-  public onSaveRole(action: IEffectsAction): Promise<IEffectsAction[]> {
-    const apiEntity = action.data as IApiEntity<IRoleEntity>;
-    return this.api.saveRole(apiEntity).then((result) => [
-      this.buildFormSubmitDoneAction(ROLE_SECTION),
-      this.buildListEntityUpdateAction(ROLES_SECTION, apiEntity, result),
-      this.buildRouterNavigateAction(ROUTER_PATHS.ROLES)
-    ]);
-  }
-
-  @EffectsService.effects(FormActionBuilder.buildSubmitErrorActionType(ROLE_SECTION))
-  public onSaveRoleError(action: IEffectsAction): IEffectsAction {
-    return this.buildNotificationErrorAction(action.error);
+  public onSaveRole(action: IEffectsAction): Promise<IRoleEntity> {
+    return this.api.saveRole(action.data as IApiEntity<IRoleEntity>);
   }
 }
